279464ee698906985f757e435febe36c77d862e0
MoRsHeD <mdmorshed0187@gmail.com>
2025-08-18 23:37:15 +0600

    allowed cors origin

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use('/api/v1', router);
 
